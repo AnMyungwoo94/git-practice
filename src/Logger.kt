3db5cbe24975ed9678a4dd33ff65db6f7a3ae6b4
AnMyungwoo94 <anmy121212@gmail.com>
2023-10-24 15:23:36 +0900

    로그용 객체를 추가한다.

---
+++ src/Logger.kt
@@ -0,0 +1,11 @@
+
+object Logger {
+    fun i(message : String){
+        println("Log Info : $message")
+    }
+
+    fun e(errorMessage : String){
+     println("Log Error : $message")
+    }
+
+}
